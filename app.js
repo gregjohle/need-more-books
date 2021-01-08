e5c74541c802178e5d38c4f706c53fa76b16b32a
Greg Johle <greg.johle@gmail.com>
2021-01-08 18:36:17 -0500

    rearranged list to scroll horizantally, fixed placement of divs

--- app.js
+++ app.js
@@ -10,18 +10,34 @@ function displayBSResults(responseJson) {
     for (let i = 0; i < responseJson.results.books.length; i++) {
         $('.js-bs-list').append(`<li class="${i}">
         <h2 class="title">${responseJson.results.books[i].title}</h2>
-        <p>By:</p>
-        <h3 class="author">${responseJson.results.books[i].author}</h3>
+        <h3 class="author"> by ${responseJson.results.books[i].author}</h3>
         <h3 class="isbn hidden">${responseJson.results.books[i].isbns[0].isbn10}</h3>
-        <img src="${responseJson.results.books[i].book_image}" alt="cover for ${responseJson.results.books[i].title}">
+        <img src="${responseJson.results.books[i].book_image}" alt="cover for ${responseJson.results.books[i].title}" class="largeImage">
         <p>${responseJson.results.books[i].description}</p>
-        <p>Would you like to see more books?</p>
         <div class="js-td-buttons">
         <button type="button" class="titleBtn">View</button>
         </div>
     </li>`);
     };
 };
+
+// Shrinks book list for viewing with book viewer
+function hideBookInfo() {
+    $('.js-bs-list').find('h2.title').addClass('hidden');
+    $('.js-bs-list').find('h3.author').addClass('hidden');
+    $('.js-bs-list').find('p').addClass('hidden');
+    $('.js-bs-list').find('img').removeClass('largeImage').addClass('smallImage');
+    $('.js-bs-list').addClass('lowerList');
+}
+
+// restores book list to full height when viewer is closed.
+function showBookInfo() {
+    $('.js-bs-list').find('h2.title').removeClass('hidden');
+    $('.js-bs-list').find('h3.author').removeClass('hidden');
+    $('.js-bs-list').find('p').removeClass('hidden');
+    $('.js-bs-list').find('img').removeClass('smallImage').addClass('largeImage');
+    $('.js-bs-list').removeClass('lowerList');
+}
 
 // Fetch the specified list from the API
 function getBS(list) {
@@ -44,10 +60,21 @@ function getBS(list) {
 // Variable used to specify which book to view
 bookIsbn = 'ISBN:0738531367'
 
+var bookNotFound = false
+
 // Alerts if book not found by specific ISBN
 function alertNotFound() {
-    alert("could not embed the book!");
+    showBookInfo();
+    $('.js-book-view').addClass('hidden');
+    alert("Could not locate that book on Google Books. Please try another book.");
 };
+
+function bookFound() {
+    if (bookNotFound === false) {
+        hideBookInfo();
+        $('.js-book-view').removeClass('hidden');
+    };
+}
 
 // Initializes viewer with ISBN
 function initialize(bookIsbn) {
@@ -58,11 +85,11 @@ function initialize(bookIsbn) {
 // Initializes viewer for selected book
 function handleViewSubmit() {
     $('.js-bs-list').on('click', '.titleBtn', function(event) {
-        $('.js-book-view').removeClass('hidden');
         let isbnText = $(this).parent().siblings('h3.isbn').text();
         bookIsbn = "ISBN:" + isbnText;
         console.log(bookIsbn);
         initialize(bookIsbn);
+        bookFound();
     });
 };
 
@@ -79,7 +106,16 @@ function handleListSubmit() {
     })
 };
 
+function handleCloseViewer() {
+    $('.js-hide-viewer').submit('.hide', function(event) {
+        event.preventDefault();
+        $('.js-book-view').addClass('hidden');
+        showBookInfo();
+    })
+}
+
 $(function() {
     handleListSubmit();
     handleViewSubmit();
+    handleCloseViewer();
 });
